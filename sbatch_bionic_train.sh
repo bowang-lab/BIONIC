3777e04f4145d7275e8297f2254b7aa0a241dca3
odatskiv <odatskiv@q.vectorinstitute.ai>
2022-12-18 18:16:23 -0500

    add sh script for bionic final train

---
+++ sbatch_bionic_train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8G
+#SBATCH --time=5:00:00
+
+EPOCHS=$1
+LEARNING_RATE=$2
+DIM=$3
+N_HEADS=$4
+N_LAYERS=$5
+LAMBDA=$6
+EXPERIMENTAL_HEAD=$7
+ATTENTION=$8
+
+cd /scratch/gobi1/odatskiv/
+
+. bionic_venv.sh
+
+cd /scratch/gobi1/odatskiv/BIONIC/
+
+echo "Running run_bionic_train.py..."
+python run_bionic_train.py -e $EPOCHS -lr $LEARNING_RATE -d $DIM -gh $N_HEADS -l $N_LAYERS -lmb $LAMBDA -head $EXPERIMENTAL_HEAD -att $ATTENTION
